9725e26beba3342414373aaae6882265ab9e6edd
khm2034 <khm2034@gmail.com>
2023-09-01 16:38:21 +0900

    VideoType Subclassing, priceCode Enum

--- src/Video.java
+++ src/Video.java
@@ -1,41 +1,25 @@
 import java.util.Date;
 
-public class Video {
+abstract public class Video {
 	private String title ;
 
-	private int priceCode ;
-	public static final int REGULAR = 1 ;
-	public static final int NEW_RELEASE =2 ;
-
-	private int videoType ;
-	public static final int VHS = 1 ;
-	public static final int CD = 2 ;
-	public static final int DVD = 3 ;
-
+	private PriceCode priceCode ;
 	private Date registeredDate ;
 	private boolean rented ;
 
-	public Video(String title, int videoType, int priceCode, Date registeredDate) {
+	public Video(String title, PriceCode priceCode, Date registeredDate) {
 		this.setTitle(title) ;
-		this.setVideoType(videoType) ;
 		this.setPriceCode(priceCode) ;
 		this.registeredDate = registeredDate ;
 	}
 
-	public int getLateReturnPointPenalty() {
-		int pentalty = 0 ;
-		switch ( videoType ) {
-			case VHS: pentalty = 1 ; break ;
-			case CD: pentalty = 2 ; break ;
-			case DVD: pentalty = 3 ; break ;
-		}
-		return pentalty ;
-	}
-	public int getPriceCode() {
+	abstract public int getLateReturnPointPenalty();
+
+	public PriceCode getPriceCode() {
 		return priceCode;
 	}
 
-	public void setPriceCode(int priceCode) {
+	public void setPriceCode(PriceCode priceCode) {
 		this.priceCode = priceCode;
 	}
 
@@ -63,20 +47,5 @@ public class Video {
 		this.registeredDate = registeredDate;
 	}
 
-	public int getVideoType() {
-		return videoType;
-	}
-
-	public void setVideoType(int videoType) {
-		this.videoType = videoType;
-	}
-
-	public int getLimit() {
-		switch ( getVideoType() ) {
-			case VHS: return 5;
-			case CD: return 3;
-			case DVD: return 2;
-		}
-		return 0;
-	}
+    abstract public int getLimit();
 }
